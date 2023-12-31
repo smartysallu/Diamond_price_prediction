f2eae2a0637faf253737f800bc0b329c2ae3f54e
smartysallu <109454250+smartysallu@users.noreply.github.com>
2023-12-31 22:33:44 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -55,13 +55,7 @@ class DataIngestion:
 
 
 
-## run Data Ingestion
-        
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
 
 
 
